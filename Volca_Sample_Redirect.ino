1c6cc29a14feaa5795df34c29428be933502e0bc
Eunjae Im <41657209+ejlabs@users.noreply.github.com>
2020-03-06 10:51:44 +0900

    Update Volca_Sample_Redirect.ino

--- Volca_Sample_Redirect.ino
+++ Volca_Sample_Redirect.ino
@@ -1,5 +1,5 @@
  /*
- * Volca Sample MIDI Channel Redirect
+ * Volca Sample MIDI Channel Redirect 6/Mar/2020
  * Simple midi channel redirect for Korg Volca Sample
  * Eunjae Im http://ejlabs.net
  */
@@ -8,35 +8,9 @@
 
 #define LED_PIN 13 // LED Pin
 
-int vPolyChan = 16; // Default Channel to send MIDI to Volca Sample
-int vNote;
-int velocity;
+int VolcaChannel = 16; // Default Channel to send MIDI to Volca Sample
 
 MIDI_CREATE_DEFAULT_INSTANCE();
-
-void handleNoteOn(byte channel, byte pitch, byte velocity) {  
-  
-  digitalWrite(LED_PIN, HIGH); // Turn ON led  
-
-  /*
-   * receiving midi note from B0(35) to G#1(44) from single midi channel (vPolyChan)
-   * and send to 1 to 10 MIDI channel to Volca Sample
-  */
-  
-  vNote = MIDI.getData1();
-  velocity = MIDI.getData2();
- 
-  if (vNote > 34 && vNote < 45 && MIDI.getChannel() == vPolyChan) {
-      MIDI.sendControlChange(7, velocity, vNote - 34); // Volca don't listen velocity, changing level instead
-      MIDI.send(MIDI.getType(), vNote, MIDI.getData2(),vNote - 34);
-  }
-  
-}
-
-void handleNoteOff(byte channel, byte pitch, byte velocity)
-{
-   digitalWrite(LED_PIN, LOW); // Turn Off led 
-}
     
 void setup(){
       
@@ -47,16 +21,56 @@ void setup(){
     MIDI.setHandleNoteOn(handleNoteOn);  // Put only the name of the function
 
     // Do the same for NoteOffs
-    MIDI.setHandleNoteOff(handleNoteOff);
- 
-    MIDI.turnThruOff();
+    MIDI.setHandleNoteOff(handleNoteOff);    
 
     // Initiate MIDI communications, listen to all channels
     MIDI.begin(MIDI_CHANNEL_OMNI);
 
+    MIDI.turnThruOff(); // Turn Thru Off
+
+}
+
+void handleNoteOn(byte channel, byte pitch, byte velocity) {
+
+  int Note;
+  int Velocity;
+  
+  digitalWrite(LED_PIN, HIGH); // Turn ON led  
+  
+  Note = MIDI.getData1();
+  Velocity = MIDI.getData2();
+  
+  if (MIDI.getChannel() == VolcaChannel) { // listen only 1 channel
+
+      /* Volca don't listen velocity so changing level instead.
+       * Comment out If you don't need it
+      */
+      MIDI.sendControlChange(7, Velocity, Note % 11);
+
+      // Sending note to each channel
+      MIDI.send(MIDI.getType(), Note, Velocity, Note % 11);
+      
+  }
+  
+}
+
+void handleNoteOff(byte channel, byte pitch, byte velocity)
+{
+   digitalWrite(LED_PIN, LOW); // Turn Off led 
 }
 
 void loop()
 {    
-    MIDI.read();
+    if (MIDI.read()) {
+
+      /* filter out all other midi message except below & note on */
+      if (MIDI.getType() == midi::Clock ||
+          MIDI.getType() == midi::Start ||
+          MIDI.getType() == midi::Continue ||
+          MIDI.getType() == midi::Stop)
+          {
+            MIDI.send(MIDI.getType(), MIDI.getData1(), MIDI.getData2(), MIDI.getChannel());
+          }
+     }
+
 }
